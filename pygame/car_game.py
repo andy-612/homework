98cbce4a8337e364f21f74d4191b9e46b4dbd011
Andy Liu <andyliu@Andys-MacBook-Pro.local>
2018-02-26 18:20:02 -0500

    race car update

--- pygame/car_game.py
+++ pygame/car_game.py
@@ -8,8 +8,9 @@ display_width = 800
 display_height = 600
 
 black = (0, 0, 0)
-white = (255, 255, 255)
+white = (255, 255, 255)q
 red = (255, 0, 0)
+green=(0, 255, 0)
 
 block_color = (53, 115, 255)
 
@@ -58,6 +59,29 @@ def crash():
     message_display('You Crashed')
 
 
+def game_intro():
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        gameDisplay.fill(white)
+        largeText = pygame.font.Font('freesansbold.ttf', 115)
+        TextSurf, TextRect = text_objects("A bit Racey", largeText)
+        TextRect.center = ((display_width / 2), (display_height / 2))
+        gameDisplay.blit(TextSurf, TextRect)
+
+        pygame.draw.rect(gameDisplay, green, (50, 450, 100, 50))
+        pygame.draw.rect(gameDisplay, red, (550, 450, 100, 50))
+
+        pygame.display.update()
+        clock.tick(15)
+
+
 def game_loop():
     x = (display_width * 0.45)
     y = (display_height * 0.8)
@@ -96,7 +120,6 @@ def game_loop():
         x += x_change
         gameDisplay.fill(white)
 
-        # things(thingx, thingy, thingw, thingh, color)
         things(thing_startx, thing_starty, thing_width, thing_height, block_color)
 
         thing_starty += thing_speed
@@ -124,6 +147,7 @@ def game_loop():
         clock.tick(60)
 
 
+game_intro()
 game_loop()
 pygame.quit()
 quit()
